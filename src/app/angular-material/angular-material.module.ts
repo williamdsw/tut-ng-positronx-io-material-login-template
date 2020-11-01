4b775eea8df5d5535fb4b5307149ab916da21190
William Santos <williamdsw@outlook.com>
2020-11-01 20:28:26 -0300

    Updating project to Angular 10.2.0, updating Angular Material, and fixing imports module

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
-import {
-  MatButtonModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatSidenavModule,
-  MatBadgeModule,
-  MatListModule,
-  MatGridListModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatSelectModule,
-  MatRadioModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatChipsModule,
-  MatTooltipModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatCardModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatCardModule } from '@angular/material/card';
 
 const MATERIAL_COMPONENTS = [
   MatButtonModule,
